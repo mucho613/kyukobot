ff6a8815cdeba4697ce58edaf15e1da0829df118
Daisuke Yamamura <mucho613@gmail.com>
2020-07-07 07:40:03 +0900

    修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,41 +16,39 @@ const client = new Twitter({
   access_token_secret: config().twitter.accesstokensecret
 });
 
-exports.scheduledFunctionCrontab = pubsub.schedule('* * * * *')
-  .timeZone('Asia/Tokyo')
-  .onRun((context) => {
-    JSDOM.fromURL(pageUrl).then((dom: any) => {
-      const emergencyInfoText = dom.window.document.querySelector(config().parser.infotextselector)
-        .textContent.trim();
+exports.scheduledFunctionCrontab = pubsub.schedule('* * * * *').onRun((context) => {
+  JSDOM.fromURL(pageUrl).then((dom: any) => {
+    const emergencyInfoText = dom.window.document.querySelector(config().parser.infotextselector)
+      .textContent.trim();
 
-      db.collection('emergencyInfoText').doc('latest').get().then(snapshot => {
-        const latestText = snapshot.data()?.text;
-        // 前回と差分がある
-        if (latestText !== emergencyInfoText) {
-          const validTweetText = (() => {
-            let trimCount = 0;
-            let trimmedText = '';
-            let trimmed = false;
-            while (true) {
-              trimmedText = emergencyInfoText.substring(0, emergencyInfoText.length - trimCount);
-              if (trimmed) trimmedText += '…';
-              const text = `${trimmedText} ${pageUrl}`;
-              if (twitterText.parseTweet(text).valid) return text;
-              trimCount++;
-              trimmed = true;
-            }
-          })();
-          client.post('statuses/update', { status: validTweetText }, (error: any, tweet: any) => {
-            if (!error) console.log(`監視結果: 差分が検出されたため、ツイートしました。\n${validTweetText}`);
-          });
-          db.collection('emergencyInfoText').doc('latest').set({
-            text: emergencyInfoText
-          }).catch(() => console.error("エラー: データベースへの書き込みに失敗しました。"));
-        }
-        // 差分がない
-        else {
-          console.log("監視結果: 差分はありません。");
-        }
-      }).catch(() => console.error("エラー: データベースからの読み込みに失敗しました。"));
-    });
+    db.collection('emergencyInfoText').doc('latest').get().then(snapshot => {
+      const latestText = snapshot.data()?.text;
+      // 前回と差分がある
+      if (latestText !== emergencyInfoText) {
+        const validTweetText = (() => {
+          let trimCount = 0;
+          let trimmedText = '';
+          let trimmed = false;
+          while (true) {
+            trimmedText = emergencyInfoText.substring(0, emergencyInfoText.length - trimCount);
+            if (trimmed) trimmedText += '…';
+            const text = `${trimmedText} ${pageUrl}`;
+            if (twitterText.parseTweet(text).valid) return text;
+            trimCount++;
+            trimmed = true;
+          }
+        })();
+        client.post('statuses/update', { status: validTweetText }, (error: any, tweet: any) => {
+          if (!error) console.log(`監視結果: 差分が検出されたため、ツイートしました。\n${validTweetText}`);
+        });
+        db.collection('emergencyInfoText').doc('latest').set({
+          text: emergencyInfoText
+        }).catch(() => console.error("エラー: データベースへの書き込みに失敗しました。"));
+      }
+      // 差分がない
+      else {
+        console.log("監視結果: 差分はありません。");
+      }
+    }).catch(() => console.error("エラー: データベースからの読み込みに失敗しました。"));
   });
+});
