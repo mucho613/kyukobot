ef98c500876db6cf767be1265a7109f83e807aa3
Daisuke Yamamura <mucho613@gmail.com>
2023-04-05 01:26:08 +0900

    update to twitter-api-v2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,30 +1,34 @@
 import { region, config } from 'firebase-functions';
-import { initializeApp, firestore } from 'firebase-admin';
+import { initializeApp } from 'firebase-admin/app';
+import { firestore } from 'firebase-admin';
+import { TwitterApi } from 'twitter-api-v2';
 
 const { JSDOM } = require('jsdom');
-const Twitter = require('twitter');
 const twitterText = require('twitter-text');
 
 initializeApp();
 const db = firestore();
 
 const pageUrl = config().parser.pageurl;
-const client = new Twitter({
-  consumer_key: config().twitter.consumerkey,
-  consumer_secret: config().twitter.consumersecret,
-  access_token_key: config().twitter.accesstokenkey,
-  access_token_secret: config().twitter.accesstokensecret
-});
+const twitterClient = new TwitterApi(config().twitter.bearertoken);
 
 exports.scheduledFunctionCrontab = region('asia-northeast1').pubsub.schedule('* * * * *').onRun((context) => {
   JSDOM.fromURL(pageUrl).then((dom: any) => {
     const emergencyInfoText = dom.window.document.querySelector(config().parser.infotextselector)
       .textContent.trim();
 
-    db.collection('emergencyInfoText').doc('latest').get().then(snapshot => {
+    db.collection('emergencyInfoText').doc('latest').get().then(async snapshot => {
       const latestText = snapshot.data()?.text;
-      // 前回と差分がある
-      if (latestText !== emergencyInfoText) {
+
+      // DB に保存された前回分のテキストと差分がない
+      if (latestText === emergencyInfoText) {
+        console.log("監視結果: 差分はありません。");
+        return 0;
+      }
+
+      // DB に保存された前回分のテキストと差分がある
+      else {
+        // Tweet できる文字数まで切り詰める
         const validTweetText = (() => {
           let trimCount = 0;
           let trimmedText = '';
@@ -38,18 +42,23 @@ exports.scheduledFunctionCrontab = region('asia-northeast1').pubsub.schedule('* 
             trimmed = true;
           }
         })();
-        client.post('statuses/update', { status: validTweetText }, (error: any, tweet: any) => {
-          if (!error) console.log(`監視結果: 差分が検出されたため、ツイートしました。\n${validTweetText}`);
-          db.collection('emergencyInfoText').doc('latest').set({ text: emergencyInfoText })
-            .then(() => console.log("監視結果: ツイートが完了したため、データベースに書き込みました。"))
-            .catch(() => console.error("エラー: データベースへの書き込みに失敗しました。"));
-        });
+
+        const { data: createdTweet } = await twitterClient.v2.tweet(validTweetText);
+
+        console.log(`監視結果: 差分が検出されたため、ツイートしました。\n${createdTweet.id}, ${createdTweet.text}`);
+
+        try {
+          await db.collection('emergencyInfoText').doc('latest').set({ text: emergencyInfoText });
+          console.log("監視結果: ツイートが完了したため、データベースに書き込みました。");
+        } catch {
+          console.error("エラー: データベースへの書き込みに失敗しました。");
+        }
+
+        return 0;
       }
-      // 差分がない
-      else console.log("監視結果: 差分はありません。");
-      return 0;
     }).catch(() => {
       console.error("エラー: データベースからの読み込みに失敗しました。")
+
       return 0;
     });
   });
