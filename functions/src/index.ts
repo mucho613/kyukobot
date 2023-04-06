f7fa2894879d7c629bcc03f906ca716281fdd483
Daisuke Yamamura <mucho613@gmail.com>
2023-04-07 02:38:17 +0900

    refactor

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,56 +10,70 @@ initializeApp();
 const db = firestore();
 
 const pageUrl = config().parser.pageurl;
-const twitterClient = new TwitterApi(config().twitter.bearertoken);
 
-exports.scheduledFunctionCrontab = region('asia-northeast1').pubsub.schedule('* * * * *').onRun((context) => {
-  JSDOM.fromURL(pageUrl).then((dom: any) => {
+exports.scheduledFunctionCrontab = region('asia-northeast1')
+  .runWith({ secrets: ["APP_KEY", "APP_KEY_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET"] })
+  .pubsub
+  .schedule('* * * * *')
+  .onRun(async () => {
+    const appKey = process.env.APP_KEY;
+    const appSecret = process.env.APP_KEY_SECRET;
+    const accessToken = process.env.ACCESS_TOKEN;
+    const accessSecret = process.env.ACCESS_TOKEN_SECRET;
+
+    if (!appKey || !appSecret || !accessToken || !accessSecret) {
+      return 0;
+    }
+
+    const twitterClient = new TwitterApi({
+      appKey: appKey,
+      appSecret: appSecret,
+      accessToken: accessToken,
+      accessSecret: accessSecret
+    });
+
+    const dom = await JSDOM.fromURL(pageUrl);
+
     const emergencyInfoText = dom.window.document.querySelector(config().parser.infotextselector)
       .textContent.trim();
 
-    db.collection('emergencyInfoText').doc('latest').get().then(async snapshot => {
-      const latestText = snapshot.data()?.text;
+    const snapshot = await db.collection('emergencyInfoText').doc('latest').get();
 
-      // DB に保存された前回分のテキストと差分がない
-      if (latestText === emergencyInfoText) {
-        console.log("監視結果: 差分はありません。");
-        return 0;
-      }
+    const latestText = snapshot.data()?.text;
 
-      // DB に保存された前回分のテキストと差分がある
-      else {
-        // Tweet できる文字数まで切り詰める
-        const validTweetText = (() => {
-          let trimCount = 0;
-          let trimmedText = '';
-          let trimmed = false;
-          while (true) {
-            trimmedText = emergencyInfoText.substring(0, emergencyInfoText.length - trimCount);
-            if (trimmed) trimmedText += '…';
-            const text = `${trimmedText} ${pageUrl}`;
-            if (twitterText.parseTweet(text).valid) return text;
-            trimCount++;
-            trimmed = true;
-          }
-        })();
+    // DB に保存された前回分のテキストと差分がない
+    if (latestText === emergencyInfoText) {
+      console.log("監視結果: 差分はありません。");
+    }
 
-        const { data: createdTweet } = await twitterClient.v2.tweet(validTweetText);
-
-        console.log(`監視結果: 差分が検出されたため、ツイートしました。\n${createdTweet.id}, ${createdTweet.text}`);
-
-        try {
-          await db.collection('emergencyInfoText').doc('latest').set({ text: emergencyInfoText });
-          console.log("監視結果: ツイートが完了したため、データベースに書き込みました。");
-        } catch {
-          console.error("エラー: データベースへの書き込みに失敗しました。");
+    // DB に保存された前回分のテキストと差分がある
+    else {
+      // Tweet できる文字数まで切り詰める
+      const validTweetText = (() => {
+        let trimCount = 0;
+        let trimmedText = '';
+        let trimmed = false;
+        while (true) {
+          trimmedText = emergencyInfoText.substring(0, emergencyInfoText.length - trimCount);
+          if (trimmed) trimmedText += '…';
+          const text = `${trimmedText} ${pageUrl}`;
+          if (twitterText.parseTweet(text).valid) return text;
+          trimCount++;
+          trimmed = true;
         }
+      })();
 
-        return 0;
+      const { data: createdTweet } = await twitterClient.v2.tweet(validTweetText);
+
+      console.log(`監視結果: 差分が検出されたため、ツイートしました。\n${createdTweet.id}, ${createdTweet.text}`);
+
+      try {
+        await db.collection('emergencyInfoText').doc('latest').set({ text: emergencyInfoText });
+        console.log("監視結果: ツイートが完了したため、データベースに書き込みました。");
+      } catch {
+        console.error("エラー: データベースへの書き込みに失敗しました。");
       }
-    }).catch(() => {
-      console.error("エラー: データベースからの読み込みに失敗しました。")
+    }
 
-      return 0;
-    });
+    return 0;
   });
-});
